41a66e9eafe39cbc408281713d74d74de58e143b
AHussain98 <106779657+AHussain98@users.noreply.github.com>
2023-12-08 08:12:13 +0000

    Update Program2.cs

--- Program2.cs
+++ Program2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Security.Cryptography.X509Certificates;
 // a class is the most common kind of reference type
 // A constant is evaluated statically at compile time, and the compiler literally substitutes its value whenever used (rather like a macro in C++).
 // A constant can be bool, char, string, any of the built-in numeric types, or an enum type.
@@ -76,7 +77,7 @@ public class Stock
 {
     decimal currentPrice; // the private "backing" field, remember C# uses private by default
 
-    public decimal CurrentPrice  // public property, decimal is a data type that is double the size and precision 
+    public decimal CurrentPrice  // public property, decimal is a data type that is double the size and precision of double
     {
         get { return currentPrice; }
         set { currentPrice = value; }  // get and set in the property can also be made private
@@ -107,7 +108,20 @@ Here’s how we could use this indexer:
 Sentence s = new Sentence();
 Console.WriteLine (s[3]); // fox
 s[3] = "kangaroo";
-Console.WriteLine (s[3]); // kangaroo */
+Console.WriteLine (s[3]); // kangaroo 
+
+ 
+ apply the readonly modifier to a struct’s functions, ensuring that if the
+function attempts to modify any field, a compile-time error is generated:
+struct Point
+{
+ public int X, Y;
+ public readonly void ResetX() => X = 0; // Error!
+} 
+If a readonly function calls a non-readonly function, the compiler generates a
+warning (and defensively copies the struct to avoid the possibility of a mutation).
+
+ */
 
 public class Test_Static
 {
@@ -124,12 +138,86 @@ public class Test_Static
 public class Asset
 {
     public string name;
+
+    public void display()
+    {
+        Console.WriteLine(this.name);   // will print the name of whichever object calls it, can be used by derived classes, this is polymorphism
+    }
+
+    public virtual Asset Clone() { return new Asset { name = name }; }  //  virtual function of Asset class 
+
+    public virtual string brag() { return "I'm an Asset!"; }
+
+    public string value() { return "Asset price could be anything!"; }
 }
 
 public class Stocks : Asset  // Stocks inherits from Asset
 {
     public long shares_owned;
 }
+
+
+// C# also has destructors called with ~, these are called finalisers
+
+// partial classes and methods can be defined over multiple source files
+
+public class House : Asset
+{
+    public long Mortgage;
+
+    public override House Clone()
+    {
+        return new House { name = name, Mortgage = Mortgage };  // covariant return type, overridden virtual function, covariant return types allow us to override a method such that it returns a more derived type
+    }
+
+    public override string brag() { return "I'm a House!"; }
+
+    public string value() { return "House prices are high right now!"; }  // this will hide the function in the base class
+}
+
+// a function marked as virtual can be overriden by subclasses wanting to provide a specialized implementation. 
+
+// A class declared as abstract can never be instantiated. Instead, only its concrete subclasses can be instantiated.
+// Abstract classes are able to define abstract members.Abstract members are like virtual members except that they don’t provide a default implementation.
+// That implementation must be provided by the subclass unless that subclass is also declared abstract:
+
+public abstract class abstract_asset {
+    public abstract decimal value { get; }  // abstract property
+}
+
+public class stock_from_abstract : abstract_asset
+{
+    public override decimal value { get { return value; } }  // overriden the abstract property from the abstract class
+}
+
+// An overridden function member can seal its implementation with the sealed keyword to prevent it from being overridden by further subclasses. 
+// sealed means it can't be overriden further
+
+
+// importantly, base class constructors always execute first 
+
+public class BaseClass {
+
+    public int X;
+    public BaseClass() { }
+    public BaseClass(int num) { X = num;  }
+
+
+}
+
+public class SubClass :  BaseClass
+{
+    int X;
+    public SubClass(int X) : base(X) { this.X = X;  Console.WriteLine(X);  Console.WriteLine(base.X); }  // you call the baseclass constructors via the base keyword
+}
+
+// object (System.Object) is the ultimate base class for all types. Any type can be upcast to object.
+// object is a reference type, by virtue of being a class. Despite this, value types, such as int, can also be cast to and from object
+// C# programs are type-checked both statically (at compile time) and at runtime (by the CLR).
+
+
+
+
 
 
 static class Program {
@@ -147,12 +235,54 @@ static class Program {
         st1.CurrentPrice = 25; // this calls set
         Console.WriteLine(st1.CurrentPrice); // this calls get 
 
+        string s = null;
+        Console.WriteLine(s?[5]);  // ? is the null operator, this means this writes nothing and will not produce an error
+
+        House h = new House();
+        Stocks st = new Stocks();
+
+        h.name = "Yard";  // assign to the public name field
+        st.name = "ADS";
+
+        h.display();  // inherited method
+        st.display();  // inherited method
+
+        Asset a = h; // upcast a to the house object h, a is only able to access the elements from h that come from the Asset class, this is object slicing
+        
+        House h2 = (House)a; // downcast from a to make it a house object and then point reference h2 to that house object
+
+        h2.name = "New Yard"; 
+        Console.WriteLine(h.name);  // now h and h2 are references to the same object
+
+        // the as operator performs a downcast that returns null if the downcast fails, this can be useful for tests
+
+        Asset b = new Asset();
+
+        b = st as Asset;  // upcast stocks st to House using as, this will return null if it fails
+
+        Console.WriteLine(b == null);  // False, upcast is successfull
+
+        // the is keyword tests if a reference conversion would succeed
+
+        Console.WriteLine(h2 is House); //True
 
 
+        Asset c = h; // point the Asset reference c to the house object
+        Console.WriteLine(c.brag());  // returns the House version of the function! virtual allows us to access the derived version of the function using a base class pointer
 
+        Asset d = new Asset();
+        House h3 = new House();
+
+        Console.WriteLine(d.value());
+        Console.WriteLine(h3.value());  // if h3 was an asset pointer pointed to a house class, it would print the house version of value(). This can be avoided via a virtual function with an override.
+
+        SubClass sc1 = new SubClass(10);
+
+        // objects have a default, built in implementation of the ToString method, which returns the type name, this can also be overwritten in the class definition
+
+        Console.WriteLine(h.ToString());  // prints house
 
 
 }
  
 }
-
